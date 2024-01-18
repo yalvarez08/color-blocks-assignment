dd8af19e3a6451aeeae73228bef515e11ac8adf0
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-01-18 15:19:23 -0500

    added onclick event for btns

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('🟥 🟦 🟩 🟨');
+
+//create function that adds red block when red btn is clicked on
+//then gets removed when red block icon is clicked
+
+function handleRedClick() {
+
+}
+
+//create function that adds blue block when blue btn is clicked on
+//then gets removed when blue block icon is clicked
+
+
+
+//create function that adds green block when green btn is clicked on
+//then gets removed when green block icon is clicked
+
+
+
+//create function that adds yellow block when yellow btn is clicked on
+//then gets removed when yellow block icon is clicked
