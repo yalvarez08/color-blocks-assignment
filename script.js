eae8f0d2e45d5bff262abeb3acac329f95dd73d8
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-01-18 15:39:30 -0500

    addedcolor functions

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ console.log('🟥 🟦 🟩 🟨');
 //then gets removed when red block icon is clicked
 
 function handleRedClick() {
+let addToDivLocation = document.querySelector('div');
 
+//take div element and replace the space with red block
+addToDivLocation.innerHTML += 
 }
 
 //create function that adds blue block when blue btn is clicked on
 //then gets removed when blue block icon is clicked
+function handleBlueClick() {
 
+}
 
 
 //create function that adds green block when green btn is clicked on
 //then gets removed when green block icon is clicked
+function handleGreenClick() {
 
+}
 
 
 //create function that adds yellow block when yellow btn is clicked on
 //then gets removed when yellow block icon is clicked
+function handleYellowClick() {
+
+}
+
+
+//function delete red
+function deleteRedBlock(){
+
+}
